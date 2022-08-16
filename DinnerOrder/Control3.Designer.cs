2ec493210f791dc0fe533a2def8d6602443f03aa
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-08-17 00:25:05 +0300

    Message added

---
+++ DinnerOrder/Control3.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace DinnerOrder
+{
+    partial class Control3
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.AdressSave = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Black", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(44, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 35);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Adres :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Black", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(44, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 35);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Telefon : ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(191, 49);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(746, 27);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(191, 131);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(746, 27);
+            this.textBox2.TabIndex = 3;
+            // 
+            // AdressSave
+            // 
+            this.AdressSave.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.AdressSave.Location = new System.Drawing.Point(126, 252);
+            this.AdressSave.Name = "AdressSave";
+            this.AdressSave.Size = new System.Drawing.Size(237, 116);
+            this.AdressSave.TabIndex = 4;
+            this.AdressSave.Text = "Adresi Kaydet ";
+            this.AdressSave.UseVisualStyleBackColor = false;
+            this.AdressSave.Click += new System.EventHandler(this.AdressSave_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button2.Location = new System.Drawing.Point(612, 252);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(228, 116);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Siparişi Tamamla";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Control3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.AdressSave);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Control3";
+            this.Size = new System.Drawing.Size(1063, 530);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button AdressSave;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ DinnerOrder/Control3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DinnerOrder
+{
+    public partial class Control3 : UserControl
+    {
+        public Control3()
+        {
+            InitializeComponent();
+        }
+
+        private void AdressSave_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Adresiniz Başarıyla Kaydedildi!");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Siparişiniz Alınmıştır, Bizi Tercih ettiğiniz için teşekkür ederiz");
+        }
+    }
+}
